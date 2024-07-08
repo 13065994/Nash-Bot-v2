d65bace24c243463ddbc75a12cf186879b3279e7
Hahsgwbhdhdj89i9 <166443104+13065994@users.noreply.github.com>
2024-07-08 21:29:17 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,13 +11,13 @@ module.exports = {
         const events = global.NashBoT.events;
         const { threadID, messageID } = event;
         
-        let commandList = "⚫️  𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗟𝗶𝘀𝘁:\n\n";
+        let commandList = "🟩⬜🟩\n𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗟𝗶𝘀𝘁:\n\n";
         let i = 0;
         commands.forEach((cmd, name) => {
-            commandList += `✪ ${++i}. ➢『 ${prefix}${name}』\n`;
+            commandList += `『 ${++i} 』. ➢ ${prefix}${name}\n`;
         });
         
-        let eventList = "⚫️ 𝗘𝘃𝗲𝗻𝘁 𝗟𝗶𝘀𝘁:\n\n";
+        let eventList = "𝗘𝘃𝗲𝗻𝘁 𝗟𝗶𝘀𝘁:\n\n";
         let j = 0;
         events.forEach((evnt, name) => {
             eventList += `✪ ${++j}. ➢『 ${name}』\n`;
